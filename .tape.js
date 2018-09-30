016c7d43aa54fcdc10ba63ec5c3bb142b8089202
Jonathan Neal <jonathantneal@hotmail.com>
2018-09-30 13:54:34 -0400

    Update tests

--- .tape.js
+++ .tape.js
@@ -1,146 +1,89 @@
-const stylelint = require('stylelint');
-const { default: { rule }, ruleName } = require('.');
-const skipBasicChecks = true;
-let accept = [], reject = [];
-
-[{
-	message: 'WARN: "left: 0" throws 1 warning',
-	sourceCSS: 'body { left: 0; }',
-	options: 'always',
-	warnings: 1,
-}, {
-	message: 'WARN: "left: 0" throws 0 warnings { except: [/^left$/i] }',
-	sourceCSS: 'body { left: 0; }',
-	options: ['always', { except: [/^left$/i] }],
-	warnings: 0,
-}, {
-	message: 'WARN: "top: 0; left: 0" throws 1 warning',
-	sourceCSS: 'body { top: 0; left: 0; }',
-	options: 'always',
-	warnings: 1
-}, {
-	message: 'WARN: "top: 0; margin-left: 0" throws 2 warnings',
-	sourceCSS: 'body { top: 0; margin-left: 0; }',
-	options: 'always',
-	warnings: 2
-}, {
-	message: 'WARN: "top: 0; margin-left: 0" throws 2 warnings',
-	sourceCSS: 'body { top: 0; margin-left: 0; }',
-	options: ['always', { except: ['top', /^margin/] }],
-	warnings: 0
-}, {
-	message: 'WARN: "padding-left: 0; margin-right: 0" throws 2 warnings',
-	sourceCSS: 'body { padding-left: 0; margin-right: 0; }',
-	options: 'always',
-	warnings: 2,
-}, {
-	message: 'WARN: "clear: left" throws 1 warning',
-	sourceCSS: 'body { clear: left; }',
-	options: 'always',
-	warnings: 1,
-}, {
-	message: 'WARN: "float: left" throws 1 warning',
-	sourceCSS: 'body { float: left; }',
-	options: 'always',
-	warnings: 1,
-}, {
-	message: 'WARN: "text-align: left" throws 1 warning',
-	sourceCSS: 'body { text-align: left; }',
-	options: 'always',
-	warnings: 1,
-}, {
-	message: 'WARN: ":dir(ltr) { top: 0; margin-left: 0; float: left; }" throws 0 warnings',
-	sourceCSS: 'body:dir(ltr) { top: 0; margin-left: 0; float: left; }',
-	options: 'always',
-	warnings: 0
-}, {
-	message: 'FIX:  "left: 0" becomes "inset-inline-start: 0"',
-	sourceCSS: 'body { left: 0; }',
-	expectCSS: 'body { inset-inline-start: 0; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "left: 0; right: 0" becomes "inset-inline: 0"',
-	sourceCSS: 'body { left: 0; right: 0; }',
-	expectCSS: 'body { inset-inline: 0; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "left: 0; top: 0" becomes "inset-start: 0"',
-	sourceCSS: 'body { left: 0; top: 0; }',
-	expectCSS: 'body { inset-start: 0; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "bottom: 0; right: 0" becomes "inset-end: 0"',
-	sourceCSS: 'body { bottom: 0; right: 0; }',
-	expectCSS: 'body { inset-end: 0; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "top: 0; right: 0; bottom: 0; left: 0" becomes "inset: 0"',
-	sourceCSS: 'body { top: 0; right: 0; bottom: 0; left: 0; }',
-	expectCSS: 'body { inset: 0; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "margin-top: 0; margin-right: 0; margin-left: 0" becomes "margin-block-start: 0; margin-inline: 0"',
-	sourceCSS: 'body { margin-top: 0; margin-right: 0; margin-left: 0; }',
-	expectCSS: 'body { margin-block-start: 0; margin-inline: 0; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "clear: left" becomes "clear: inline-start"',
-	sourceCSS: 'body { clear: left; }',
-	expectCSS: 'body { clear: inline-start; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "float: right" becomes "float: inline-end"',
-	sourceCSS: 'body { float: right; }',
-	expectCSS: 'body { float: inline-end; }',
-	options: 'always'
-}, {
-	message: 'FIX:  "text-align: left" becomes "text-align: start"',
-	sourceCSS: 'body { text-align: left; }',
-	expectCSS: 'body { text-align: start; }',
-	options: 'always'
-}, {
-	message: 'FIX:  ":dir(ltr) { text-align: left }" remains unchanged',
-	sourceCSS: 'body:dir(ltr) { text-align: left; }',
-	expectCSS: 'body:dir(ltr) { text-align: left; }',
-	options: ['always']
-}, {
-	message: 'FIX:  "float: left; text-align: left" becomes "float: left; text-align: start" { except: [/^float$/i] }',
-	sourceCSS: 'body { float: left; text-align: start; }',
-	options: ['always', {
-		except: [/^float$/i]
+module.exports = {
+	'csstools/use-logical': [{
+		source: 'body { left: 0 }',
+		args: 'always',
+		warnings: 1,
+	}, {
+		source: 'body { left: 0 }',
+		args: ['always', { except: 'left' }],
+		warnings: 0,
+	}, {
+		source: 'body { top: 0; left: 0 }',
+		args: 'always',
+		warnings: 1
+	}, {
+		source: 'body { top: 0; margin-left: 0 }',
+		args: 'always',
+		warnings: [ 'Unexpected "top" property.', 'Unexpected "margin-left" property.' ]
+	}, {
+		source: 'body { top: 0; margin-left: 0 }',
+		args: ['always', { except: ['top', /^margin/] }],
+		warnings: 0
+	}, {
+		source: 'body { padding-left: 0; margin-right: 0 }',
+		args: 'always',
+		warnings: 2,
+	}, {
+		source: 'body { clear: left }',
+		args: 'always',
+		warnings: 1,
+	}, {
+		source: 'body { float: left }',
+		args: 'always',
+		warnings: 1,
+	}, {
+		source: 'body { text-align: left }',
+		args: 'always',
+		warnings: 1,
+	}, {
+		source: 'body:dir(ltr) { top: 0; margin-left: 0; float: left }',
+		args: 'always',
+		warnings: 0
+	}, {
+		source: 'body { left: 0 }',
+		expect: 'body { inset-inline-start: 0 }',
+		args: 'always'
+	}, {
+		source: 'body { left: 0; right: 0 }',
+		expect: 'body { inset-inline: 0 }',
+		args: 'always'
+	}, {
+		source: 'body { left: 0; top: 0 }',
+		expect: 'body { inset-start: 0 }',
+		args: 'always'
+	}, {
+		source: 'body { bottom: 0; right: 0 }',
+		expect: 'body { inset-end: 0 }',
+		args: 'always'
+	}, {
+		source: 'body { top: 0; right: 0; bottom: 0; left: 0 }',
+		expect: 'body { inset: 0 }',
+		args: 'always'
+	}, {
+		source: 'body { margin-top: 0; margin-right: 0; margin-left: 0 }',
+		expect: 'body { margin-block-start: 0; margin-inline: 0 }',
+		args: 'always'
+	}, {
+		source: 'body { clear: left }',
+		expect: 'body { clear: inline-start }',
+		args: 'always'
+	}, {
+		source: 'body { float: right }',
+		expect: 'body { float: inline-end }',
+		args: 'always'
+	}, {
+		source: 'body { text-align: left }',
+		expect: 'body { text-align: start }',
+		args: 'always'
+	}, {
+		source: 'body:dir(ltr) { text-align: left }',
+		expect: 'body:dir(ltr) { text-align: left }',
+		args: ['always']
+	}, {
+		source: 'body { float: left; text-align: left }',
+		expect: 'body { float: left; text-align: start }',
+		args: ['always', {
+			except: [/^float$/i]
+		}]
 	}]
-}].reduce(
-	(promise, schema) => promise.then(() => console.log(`- ${schema.message}`) || test(schema)),
-	Promise.resolve()
-).then(
-	process.exit.bind(process, 0),
-	error => console.error(`${error.message}`) || process.exit(1)
-);
-
-function test(config) {
-	return stylelint.lint({
-		code: config.sourceCSS,
-		config: {
-			plugins: ['.'],
-			rules: {
-				[ruleName]: config.options
-			}
-		},
-		fix: Boolean(config.expectCSS)
-	}).then(results => {
-		const warnings = results.results.reduce(
-			(array, result) => array.concat(result.warnings),
-			[]
-		);
-
-		if (warnings.length) {
-			if (typeof config.warnings === 'number' && config.warnings !== warnings.length) {
-				throw new Error(`Expected ${config.warnings} warnings\nReceived ${warnings.length} warnings`);
-			}
-		}
-
-		if (config.expectCSS && results.output !== config.expectCSS) {
-			throw new Error(`Expected: ${config.expectCSS}\nRecieved: ${results.output}`);
-		}
-	});
-}
+};
